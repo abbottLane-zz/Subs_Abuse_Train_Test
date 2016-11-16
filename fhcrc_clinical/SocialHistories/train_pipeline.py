504b92564715f74cc9a8789383cf0ff08db572ad
Will <will@asklane.com>
2016-11-16 10:27:57 -0800

    minor import changes

--- fhcrc_clinical/SocialHistories/train_pipeline.py
+++ fhcrc_clinical/SocialHistories/train_pipeline.py
@@ -1,7 +1,7 @@
 # train classifiers and extractors and output models
 import time
 
-import DataLoading.DataLoader
+from fhcrc_clinical.SocialHistories.DataLoading import DataLoader
 from fhcrc_clinical.SocialHistories.Extraction.EventDetection import Training as EventDetectionTraining
 from fhcrc_clinical.SocialHistories.Extraction.StatusClassification import Training as StatusClassificationTraining
 from fhcrc_clinical.SocialHistories.Extraction.EventAttributeLinking import Training as EventFillerTraining
@@ -10,10 +10,10 @@ from fhcrc_clinical.SocialHistories.SystemUtilities.Stopwatch import stopWatch
 
 
 def main():
-    start = time.time()  # What in other posts is described is
+    start = time.time()  # Start a timer to see how long training takes
 
     # Load Data
-    patients = DataLoading.DataLoader.main("Train")  # list of filled Patient objects
+    patients = DataLoader.main("Train")  # list of filled Patient objects
 
     # Event Detection
     EventDetectionTraining.train_event_detectors(patients)
