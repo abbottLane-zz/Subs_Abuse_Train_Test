3d1c25a8d802101230d8168f113fa653c9317c16
wlane <will@asklane.com>
2016-11-16 09:37:25 -0800

    added 169 to training set, gitignore to cover model dirs

--- fhcrc_clinical/SocialHistories/SystemUtilities/Configuration.py
+++ fhcrc_clinical/SocialHistories/SystemUtilities/Configuration.py
@@ -2,6 +2,7 @@
 import os
 
 # Data directories
-DATA_DIR = os.path.join(os.getcwd(),"fhcrc_clinical","SocialHistories", "DataDir")
+cur_dir_path = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(cur_dir_path, "..", "DataDir")
 MODEL_DIR = os.path.join(DATA_DIR, "SVMModels")
 ATTRIB_EXTRACTION_DIR_HOME = os.path.join(DATA_DIR, "CRFModels")
